2cfaf7592adcaef1be51449972178616482222ca
Diego <malucorp@gmail.com>
2019-04-30 15:09:08 -0700

    Change front end to jquery

--- js/main.js
+++ js/main.js
@@ -44,8 +44,6 @@
 		menuCtrl = document.getElementById('menu-toggle'),
 		menuCloseCtrl = sidebarEl.querySelector('.close-button');
 
-		proximo = contentItemsContainer.querySelectorAll('.proximo');
-		anterior = contentItemsContainer.querySelectorAll('.anterior');
 
 	/**
 	 * gets the viewport width and height
@@ -86,9 +84,11 @@
 				// index of current item
 				current = pos;
 				// simulate loading time..
-				classie.add(item, 'grid__item--loading');
+				//classie.add(item, 'grid__item--loading');
+				$(item).addClass('grid__item--loading');
 				setTimeout(function() {
-					classie.add(item, 'grid__item--animate');
+					//classie.add(item, 'grid__item--animate');
+					$(item).addClass('grid__item--animate');
 					// reveal/load content after the last element animates out (todo: wait for the last transition to finish)
 					setTimeout(function() { loadContent(item); }, 500);
 				}, 1000);
@@ -100,54 +100,81 @@
 			hideContent();
 		});
 
-		//botoes next
-		[].slice.call(proximo).forEach(function(item, pos) {
+		//botao next
+/*		[].slice.call(proximo).forEach(function(item, pos) {
 			item.addEventListener('click', function(ev) {
 				ev.preventDefault();
-				var contentItem = contentItems[current];
-				var gridItem = gridItems[current];
+				var contentItem = contentItems[current], gridItem = gridItems[current];
 				current = pos;
-				//alterar o numero, mesmo numero para posts/jobs
+				//alterar o numero, mesmo numero para posts/jobs -1
 				if(current !== 9) {
 					//console.log(current);
-					//console.log('frente / pos: '+pos+ ' current: '+current)
-					classie.add(gridItems[current+1], 'grid__item--loading');
-					classie.add(gridItems[current+1], 'grid__item--animate');
-					classie.removeClass(gridItems[current], 'grid__item--loading');
-					classie.removeClass(gridItems[current], 'grid__item--animate');
-					classie.add(contentItems[current + 1], 'content__item--show');
-					classie.removeClass(contentItems[current], 'content__item--show');
+					//console.log(contentItem);
+					//console.log(gridItem);
+					//console.log('frente / pos: '+pos+ ' current: '+current+ ' contentItem: '+contentItem);
+
+					//classie.add(gridItems[current+1], 'grid__item--loading');
+					//classie.add(gridItems[current+1], 'grid__item--animate');
+					$(gridItems[current+1]).addClass('grid__item--loading grid__item--animate')
+					
+					//classie.removeClass(gridItems[current], 'grid__item--loading');
+					//classie.removeClass(gridItems[current], 'grid__item--animate');
+					$(gridItems[current]).removeClass('grid__item--loading grid__item--animate')
+					
+					//classie.add(contentItems[current + 1], 'content__item--show');
+					$(contentItems[current + 1]).addClass('content__item--show');
+					
+					//classie.removeClass(contentItems[current], 'content__item--show');
+					$(contentItems[current]).removeClass('content__item--show');
+					console.log('next0');
 				} else {
 					//console.log('fim / pos: '+pos+ ' current: '+current)
 					//desabilitar o botão
-					classie.add(item,'disabled');
+					$(item).addClass('disabled');
+					console.log('next1');
 				}
 			})
-
 		});
-		//botoes previous
-		[].slice.call(anterior).forEach(function(item, pos) {
-			item.addEventListener('click', function(ev) {
-				ev.preventDefault();
-				var contentItem = contentItems[current];
-				current = pos;
-				if(current !== 0) {
-					//console.log(current);
-					classie.add(gridItems[current-1], 'grid__item--loading');
-					classie.add(gridItems[current-1], 'grid__item--animate');
-					classie.removeClass(gridItems[current], 'grid__item--loading');
-					classie.removeClass(gridItems[current], 'grid__item--animate');
 
-					classie.add(contentItems[current - 1], 'content__item--show');
-					classie.removeClass(contentItems[current], 'content__item--show');
+		//botao previous
+		[].slice.call(anterior).forEach(function(item, pos) {
+			console.log('previous0');
+			item.addEventListener('click', function(ev) {
+				console.log('previous0');
+				ev.preventDefault();
+				var contentItem = contentItems[current], gridItem = gridItems[current];
+				current = pos;
+				if(current !== -0) {
+					//console.log(current);
+					//classie.add(gridItems[current-1], 'grid__item--loading');
+					//classie.add(gridItems[current-1], 'grid__item--animate');
+					$(gridItems[current-1]).addClass('grid__item--loading grid__item--animate');
+					
+					//classie.removeClass(gridItems[current], 'grid__item--loading');
+					//classie.removeClass(gridItems[current], 'grid__item--animate');
+					$(gridItems[current]).removeClass('grid__item--loading grid__item--animate');
+
+					//classie.add(contentItems[current - 1], 'content__item--show');
+					$(contentItems[current - 1]).addClass('content__item--show');
+					
+					//classie.removeClass(contentItems[current], 'content__item--show');
+					$(contentItems[current]).removeClass('content__item--show');
+					console.log('previous0');
 				}else{
 					//desabilitar o botao 
-					classie.add(item,'disabled');
+					$(item).addClass('disabled');
+					console.log('previous1');
 				}
 			})
-		})
-
-
+		});
+		
+		
+/*		var currentItem = $('article.content__item');
+		$.get('./jobs/job_'+current+'.html?asdas', function(respons) {
+			
+			$(currentItem).html(respons);// = $.parseHTML(respons);
+			console.log(currentItem);
+		});  */
 
 		// keyboard esc - hide content
 		document.addEventListener('keydown', function(ev) {
@@ -161,23 +188,23 @@
 				}
 			}
 		} );
-
+		
 		// hamburger menu button (mobile) and close cross
 		menuCtrl.addEventListener('click', function() {
-			if( !classie.has(sidebarEl, 'sidebar--open') ) {
-				classie.add(sidebarEl, 'sidebar--open');	
+			if( !$(sidebarEl).hasClass('sidebar--open') /*classie.has(sidebarEl, 'sidebar--open')*/ ) {
+				//classie.add(sidebarEl, 'sidebar--open');	
+				$(sidebarEl).addClass('sidebar--open');
 			}
 		});
 
 		menuCloseCtrl.addEventListener('click', function() {
-			if( classie.has(sidebarEl, 'sidebar--open') ) {
-				classie.remove(sidebarEl, 'sidebar--open');
+			if( $(sidebarEl).hasClass('sidebar--open') /*classie.has(sidebarEl, 'sidebar--open')*/ ) {
+				//classie.remove(sidebarEl, 'sidebar--open');
+				$(sidebarEl).removeClass('sidebar--open');
+				
 			}
 		});
 	}
-
-
-	
 
 	function loadContent(item) {
 		// add expanding element/placeholder 
@@ -189,13 +216,83 @@
 		dummy.style.transform = 'translate3d(' + (item.offsetLeft - 5) + 'px, ' + (item.offsetTop - 5) + 'px, 0px) scale3d(' + item.offsetWidth/gridItemsContainer.offsetWidth + ',' + item.offsetHeight/getViewport('y') + ',1)';
 
 		// add transition class 
-		classie.add(dummy, 'placeholder--trans-in');
+		$(dummy).addClass('placeholder--trans-in');
 
 		// insert it after all the grid items
 		gridItemsContainer.appendChild(dummy);
 		
+		//botao next
+		function next(){
+			proximo = contentItemsContainer.querySelectorAll('.proximo');
+			[].slice.call(proximo).forEach(function(item) {
+				item.addEventListener('click', function(ev) {
+					ev.preventDefault();
+					//remove a classe do item atual
+					//$(currentItem).removeClass( "content__item--show" );
+					if(current !== 10) {
+						$(contentItemsContainer).removeClass('content--show');
+						$(gridItems[current+1]).addClass('grid__item--loading grid__item--animate');
+						$(gridItems[current]).removeClass('grid__item--loading grid__item--animate');
+						$(contentItems[current + 1]).addClass('content__item--show');
+						$(contentItems[current]).removeClass('content__item--show');
+						current = ++current;	
+						$.get('./jobs/job_'+ current+'.html?', function(respons) {
+							$(currentItem).html(respons);
+							//faz o efeito do proximo item
+							$(currentItem).addClass( "content__item--show" );
+							$(contentItemsContainer).addClass('content--show');
+							next();
+							previous();
+						});		
+					} else {
+						//desabilitar o botão
+						$(item).addClass('disabled');
+					}
+					console.log("current é final é: "+current);
+				});
+			});
+		};
+		//botao previous
+		function previous(){
+			anterior = contentItemsContainer.querySelectorAll('.anterior');
+			[].slice.call(anterior).forEach(function(item) {
+				item.addEventListener('click', function(ev) {
+					ev.preventDefault();
+					if(current !== 0) {
+						$(contentItemsContainer).removeClass('content--show');
+						$(gridItems[current-1]).addClass('grid__item--loading grid__item--animate');
+						$(gridItems[current]).removeClass('grid__item--loading grid__item--animate');
+						$(contentItems[current - 1]).addClass('content__item--show');
+						$(contentItems[current]).removeClass('content__item--show');
+						current = --current;	
+						$.get('./jobs/job_'+ current+'.html?', function(respons) {
+							$(currentItem).html(respons);
+							//faz o efeito do proximo item
+							$(currentItem).addClass( "content__item--show" );
+							$(contentItemsContainer).addClass('content--show');
+							previous();
+							next();
+						});		
+					} else {
+						//desabilitar o botao 
+						$(item).addClass('disabled');
+					}
+					console.log("current é final é: "+current);
+				})
+			});
+		};
+		
 		// body overlay
-		classie.add(bodyEl, 'view-single');
+		$(bodyEl).addClass('view-single');
+		var currentItem = $('article.content__item');
+		$.get('./jobs/job_'+current+'.html?', function(respons) {
+			
+			$(currentItem).html(respons);// = $.parseHTML(respons);
+			console.log(currentItem);			
+			next();
+			previous();
+			
+		});
 
 		setTimeout(function() {
 			// expands the placeholder
@@ -207,58 +304,57 @@
 
 		onEndTransition(dummy, function() {
 			// add transition class 
-			classie.remove(dummy, 'placeholder--trans-in');
-			classie.add(dummy, 'placeholder--trans-out');
+			$(dummy).removeClass('placeholder--trans-in').addClass('placeholder--trans-out');
 			// position the content container
 			contentItemsContainer.style.top = scrollY() + 'px';
+
 			// show the main content container
-			classie.add(contentItemsContainer, 'content--show');
+			$(contentItemsContainer).addClass('content--show');
+			
 			// show content item:
-			classie.add(contentItems[current], 'content__item--show');
+			$( currentItem ).addClass( "content__item--show" );
+			
 			// show close control
-			classie.add(closeCtrl, 'close-button--show');
+			$(closeCtrl).addClass('close-button--show');
 			// sets overflow hidden to the body and allows the switch to the content scroll
-			classie.addClass(bodyEl, 'noscroll');
+			$(bodyEl).addClass('noscroll');
 
 			isAnimating = false;
 		});
 	}
 
 	function hideContent() {
-		var gridItem = gridItems[current], contentItem = contentItems[current];
+		//var currentItem = $('#'+ --current);
+		//var gridItem = gridItems[current], contentItem = contentItems[current];
+		var gridItem = gridItems[current], contentItem = $('article.content__item');// contentItems[current];
 
-		[].slice.call(contentItems).forEach(function(item, pos) {
-			current = pos;
-			classie.removeClass(contentItems[current], 'content__item--show');
-			classie.removeClass(gridItems[current], 'grid__item--loading');
-			classie.removeClass(gridItems[current], 'grid__item--animate');
-
-		})
-
-		classie.removeClass(contentItem, 'content__item--show');
-		classie.removeClass(contentItemsContainer, 'content--show');
-		classie.removeClass(closeCtrl, 'close-button--show');
-		classie.removeClass(bodyEl, 'view-single');
+		$(contentItem).removeClass('content__item--show');
+		$(contentItemsContainer).removeClass('content--show');
+		$(closeCtrl).removeClass('close-button--show');
+		$(bodyEl).removeClass('view-single');
+		
 
 		setTimeout(function() {
 			var dummy = gridItemsContainer.querySelector('.placeholder');
-
-			classie.removeClass(bodyEl, 'noscroll');
+			$(bodyEl).removeClass('noscroll');
 
 			dummy.style.WebkitTransform = 'translate3d(' + gridItem.offsetLeft + 'px, ' + gridItem.offsetTop + 'px, 0px) scale3d(' + gridItem.offsetWidth/gridItemsContainer.offsetWidth + ',' + gridItem.offsetHeight/getViewport('y') + ',1)';
 			dummy.style.transform = 'translate3d(' + gridItem.offsetLeft + 'px, ' + gridItem.offsetTop + 'px, 0px) scale3d(' + gridItem.offsetWidth/gridItemsContainer.offsetWidth + ',' + gridItem.offsetHeight/getViewport('y') + ',1)';
 
 			onEndTransition(dummy, function() {
 				// reset content scroll..
-				contentItem.parentNode.scrollTop = 0;
+				//contentItem.parentNode.scrollTop = 0;
 				gridItemsContainer.removeChild(dummy);
-				classie.remove(gridItem, 'grid__item--loading');
-				classie.remove(gridItem, 'grid__item--animate');
-				//console.log(current);*/
+				$(gridItem).removeClass('grid__item--loading grid__item--animate');
 				lockScroll = false;
 				window.removeEventListener( 'scroll', noscroll );
+				/*remove todas as classes do site quando fecha, funciona quando não chama por ajax os jobs */
+				/*[].slice.call(contentItems).forEach(function(item, pos) {
+					current = pos;
+					$(gridItem).removeClass('content__item--show');
+					$(gridItem).removeClass('grid__item--loading grid__item--animate');
+				})*/
 			});
-			
 			// reset current
 			current = -1;
 		}, 300);
